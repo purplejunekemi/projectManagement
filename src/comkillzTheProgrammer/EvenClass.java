b8c2990034a4ab27c8ab1d4fc3eb72325a40b2df
skillzTheProgrammer <skillztheprogrammer@gmail.com>
2020-02-11 17:35:23 +0100

    myAll out app
    my first commit

---
+++ src/comkillzTheProgrammer/EvenClass.java
@@ -0,0 +1,10 @@
+package comkillzTheProgrammer;
+
+public class EvenClass {
+
+    public void evenPrint(int num){
+        for (int i =0; i< num; i = i + 2){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/comkillzTheProgrammer/Main.java
@@ -0,0 +1,10 @@
+package comkillzTheProgrammer;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        EvenClass evenClass = new EvenClass();
+        evenClass.evenPrint(100);
+    }
+}
